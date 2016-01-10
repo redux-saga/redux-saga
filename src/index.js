30a7f53e61312d07dcd8a65eeff02d79cccdf5a1
yelouafi <yelouafi@gmail.com>
2016-01-10 01:02:37 +0000

    export cancellation related types

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ export default middleware
 
 export {
   SagaCancellationException,
-  CANCEL as CANCEL_PROMISE,
   RACE_AUTO_CANCEL,
   PARALLEL_AUTO_CANCEL,
   MANUAL_CANCEL
 } from './proc'
+
 export { take, put, race, call, cps, fork, join, cancel, as } from './io'
 export { runSaga, storeIO } from './runSaga'
