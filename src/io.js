2157a1c313c762c35ffb0ff7840eee807400a7e6
yelouafi <yelouafi@gmail.com>
2016-01-15 12:54:38 +0000

    check undefined argument in io.take

--- src/io.js
+++ src/io.js
@@ -6,6 +6,7 @@ export const CPS_FUNCTION_ARG_ERROR = "cps first argument must be a function"
 export const FORK_ARG_ERROR = "fork first argument must be a generator function or an iterator"
 export const JOIN_ARG_ERROR = "join argument must be a valid task (a result of a fork)"
 export const CANCEL_ARG_ERROR = "cancel argument must be a valid task (a result of a fork)"
+export const INVALID_PATTERN = "Invalid pattern passed to `take` (HINT: check if you didn't mispell a constant)"
 
 
 const IO    = Symbol('IO')
@@ -37,6 +38,10 @@ export function matcher(pattern) {
 }
 
 export function take(pattern){
+  if (arguments.length > 0 && is.undef(pattern)) {
+    throw new Error(INVALID_PATTERN)
+  }
+
   return effect(TAKE, is.undef(pattern) ? '*' : pattern)
 }
 
