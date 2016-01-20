1c8be6b040be1acb5707212cf8bf2234eecae303
yelouafi <yelouafi@gmail.com>
2016-01-20 18:22:25 +0000

    added monitorActions export

--- src/index.js
+++ src/index.js
@@ -8,5 +8,9 @@ export {
   MANUAL_CANCEL
 } from './proc'
 
+export { is } from './utils'
 export { take, put, race, call, apply, cps, fork, join, cancel, as } from './io'
 export { runSaga, storeIO } from './runSaga'
+
+import * as monitorActions from './monitorActions'
+export  { monitorActions }
