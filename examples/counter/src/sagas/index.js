1675726663cfdb2122a92bd1ffbc13479a0e9f77
yelouafi <yelouafi@gmail.com>
2016-02-03 17:36:26 +0000

    cancel async increment with race

--- examples/counter/src/sagas/index.js
+++ examples/counter/src/sagas/index.js
@@ -1,33 +1,24 @@
 /* eslint-disable no-constant-condition */
 
-import { take, put, call, fork, race, cancel } from '../../../../src'
+import { take, put, call, fork, race } from '../../../../src'
 import { INCREMENT_ASYNC, INCREMENT_COUNTER, CANCEL_INCREMENT_ASYNC } from '../constants'
 import { delay } from '../services'
 import { increment, showCongratulation } from '../actions/counter'
 
 export function* incrementAsync() {
-
-  // call delay : Number -> Promise
   yield call(delay, 1000)
-
-  // dispatch INCREMENT_COUNTER
   yield put(increment())
-
 }
 
 export function* watchIncrementAsync() {
-  let task
-
-  // wait for each INCREMENT_ASYNC action
   while(yield take(INCREMENT_ASYNC)) {
     // starts a 'Race' between an async increment and a user cancel action
     // if user cancel action wins, the incrementAsync will be cancelled automatically
-    task = yield fork(incrementAsync)
-
-    yield take(CANCEL_INCREMENT_ASYNC)
-    yield cancel(task)
+    yield race([
+      call(incrementAsync),
+      take(CANCEL_INCREMENT_ASYNC)
+    ])
   }
-
 }
 
 export function* onBoarding() {
