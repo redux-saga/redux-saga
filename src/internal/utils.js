b412d8cb2c7031132290d71b4755ca258d5a0e03
yelouafi <yelouafi@gmail.com>
2016-02-12 19:07:25 +0000

    drop unused SAGA_ITERATOR symbol

--- src/internal/utils.js
+++ src/internal/utils.js
@@ -1,5 +1,4 @@
 export const TASK  = Symbol('TASK')
-export const SAGA_ITERATOR = Symbol('SAGA_ITERATOR')
 export const kTrue = () => true
 export const noop = () => {}
 
@@ -16,7 +15,7 @@ export const is = {
   func      : f => typeof f === 'function',
   array     : Array.isArray,
   promise   : p => p && is.func(p.then),
-  iterator  : it => it && ( it[SAGA_ITERATOR] || (is.func(it.next) && is.func(it[Symbol.iterator])) ),
+  iterator  : it => it && is.func(it.next) && is.func(it[Symbol.iterator]) ,
   throw     : it => it && is.func(it.throw),
   task      : it => it && it[TASK]
 }
