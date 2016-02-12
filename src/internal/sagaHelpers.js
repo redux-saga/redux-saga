afdeee0608e8d25d1285ef84d1b2cfdf3a502079
yelouafi <yelouafi@gmail.com>
2016-02-12 14:30:29 +0000

    manual impl. of higher level API using fsm

--- src/internal/sagaHelpers.js
+++ src/internal/sagaHelpers.js
@@ -1,24 +1,62 @@
 /* eslint-disable no-constant-condition */
 
+import { is, SAGA_ITERATOR } from './utils'
 import { take, fork, cancel } from './io'
+import SagaCancellationException from './SagaCancellationException'
+
+const resume = (fnOrValue, arg) => is.func(fnOrValue) ? fnOrValue(arg) : fnOrValue
+const done = { done: true }
+
+function fsmIterator(fsm, nextState) {
+  let aborted, updateState
+
+  function next(arg, error) {
+    if(aborted)
+      return  done
+
+    if(error) {
+      aborted = true
+      if(!(error instanceof SagaCancellationException))
+        throw error
+      return done
+    } else {
+      if(updateState)
+        updateState(arg)
+
+      const [output, transition, _updateState] = fsm[nextState]
+      updateState = _updateState
+      nextState = resume(transition, arg)
+      return resume(output, arg)
+    }
+  }
+
+  return {
+    [SAGA_ITERATOR]: true,
+    next,
+    throw: error => next(null, error)
+  }
+}
 
 export function takeEvery(pattern, worker) {
-  return function* __takeEvery_watcher() {
-    while(true) {
-      const action = yield take(pattern)
-      yield fork(worker, action)
-    }
-  }()
+  const yieldTake = { done: false, value: take(pattern)}
+  const yieldFork = action => ({ done: false, value: fork(worker, action)})
+
+  return fsmIterator({
+    'take' : [yieldTake, 'fork'],
+    'fork' : [yieldFork, 'take']
+  }, 'take')
 }
 
 export function takeLatest(pattern, worker) {
-  return function* __takeEvery_watcher() {
-    let currentTask
-    while(true) {
-      const action = yield take(pattern)
-      if(currentTask)
-        yield cancel(currentTask)
-      currentTask = yield fork(worker, action)
-    }
-  }()
+  const yieldTake   = { done: false, value: take(pattern)}
+  const yieldFork   = () => ({ done: false, value: fork(worker, currentAction)})
+  const yieldCancel = () => ({ done: false, value: cancel(currentTask)})
+  const forkOrCancel = () => currentTask ? 'cancel' : 'fork'
+
+  let currentTask, currentAction
+  return fsmIterator({
+    'take'   : [ yieldTake, forkOrCancel, action => currentAction = action ],
+    'cancel' : [yieldCancel, 'fork'],
+    'fork'   : [yieldFork, 'take', task => currentTask = task ]
+  }, 'take')
 }
