62f705de8b9fd3553139851cb04441b4f8554c74
yelouafi <yelouafi@gmail.com>
2016-02-12 14:31:56 +0000

    added specs for cancelling watchers

--- test/sagaHelpers/takeEvery.js
+++ test/sagaHelpers/takeEvery.js
@@ -2,16 +2,28 @@
 
 import test from 'tape';
 import sagaMiddleware, { takeEvery } from '../../src'
+import { take, fork, cancel } from '../../src/effects'
 import { createStore, applyMiddleware } from 'redux'
 
+const delay = ms => new Promise(r => setTimeout(r, ms))
 
 test('takeEvery', assert => {
   assert.plan(1)
 
+  const DELAY = 5
+  const loop = 10
+
   const actual = []
-  const store = applyMiddleware(sagaMiddleware(watcher))(createStore)(() => {})
+  const store = applyMiddleware(sagaMiddleware(root))(createStore)(() => {})
+
+  function* root() {
+    const task = yield  fork(watcher)
+    yield take('CANCEL_WATCHER')
+    yield cancel(task)
+  }
 
   function* worker(action) {
+    yield delay(DELAY)
     actual.push(action.payload)
   }
 
@@ -21,13 +33,20 @@ test('takeEvery', assert => {
 
   Promise.resolve(1)
   .then(() => {
-    store.dispatch({type: 'ACTION', payload: 1})
-    store.dispatch({type: 'ACTION', payload: 2})
-    store.dispatch({type: 'ACTION', payload: 3})
+    for(let i = 1; i <= loop/2; i++)
+      store.dispatch({type: 'ACTION', payload: i})
   })
+  // the watcher should be cancelled after this
+  // no further task should be forked after this
+  .then(() => store.dispatch({type: 'CANCEL_WATCHER'}) )
   .then(() => {
-    assert.deepEqual(actual, [1,2,3],
+    for(let i = loop/2 + 1; i <= loop; i++)
+      store.dispatch({type: 'ACTION', payload: i})
+  })
+
+  setTimeout(() => {
+    assert.deepEqual(actual, [1,2,3,4,5],
       "takeEvery must fork a worker on each action"
     );
-  })
+  }, DELAY * loop + 10)
 })
--- test/sagaHelpers/takeLatest.js
+++ test/sagaHelpers/takeLatest.js
@@ -4,15 +4,21 @@ import test from 'tape';
 import sagaMiddleware, { takeLatest } from '../../src'
 import { createStore, applyMiddleware } from 'redux'
 import { arrayOfDeffered } from '../../src/utils'
+import { take, fork, cancel } from '../../src/effects'
 
-
-test('takeEvery', assert => {
+test('takeLatest', assert => {
   assert.plan(1)
 
   const defs = arrayOfDeffered(4)
 
   const actual = []
-  const store = applyMiddleware(sagaMiddleware(watcher))(createStore)(() => {})
+  const store = applyMiddleware(sagaMiddleware(root))(createStore)(() => {})
+
+  function* root() {
+    const task = yield fork(watcher)
+    yield take('CANCEL_WATCHER')
+    yield cancel(task)
+  }
 
   function* worker(action) {
     const idx = action.payload - 1
@@ -31,12 +37,26 @@ test('takeEvery', assert => {
   .then(() => store.dispatch({type: 'ACTION', payload: 3}) )
   .then(() => defs[1].resolve('w-2') )
   .then(() => defs[2].resolve('w-3') )
-  .then(() => store.dispatch({type: 'ACTION', payload: 4}) )
+  .then(() => {
+    store.dispatch({type: 'ACTION', payload: 4})
+    /*
+      We immediately cancel the watcher after firing the action
+      The watcher should be canceleld after this
+      no furhter task should be forked
+      but the last forked task must keep running
+    */
+    store.dispatch({type: 'CANCEL_WATCHER'})
+  })
   .then(() => defs[3].resolve('w-4') )
+  .then(() => {
+    // this one should be ignored by the watcher
+    store.dispatch({type: 'ACTION', payload: 5})
+  })
 
   .then(() => {
     assert.deepEqual(actual, ['w-3', 'w-4'],
-      "takeEvery must fork a worker on each action"
+      "takeLatest must cancel current task before forking a new task"
     );
   })
+  .catch(err => assert.fail(err))
 })
