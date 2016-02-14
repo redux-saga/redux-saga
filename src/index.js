370cb628bae1d6610c321b3340052ba038c2b7eb
yelouafi <yelouafi@gmail.com>
2016-02-14 17:58:42 +0000

    renamed isCancel to isCancelError

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export default middleware
 
 import _SagaCancellationException from './internal/SagaCancellationException'
 export const SagaCancellationException = _SagaCancellationException
-export const isCancel = error => error instanceof SagaCancellationException
+export const isCancelError = error => error instanceof SagaCancellationException
 
 export { runSaga, storeIO } from './internal/runSaga'
 
