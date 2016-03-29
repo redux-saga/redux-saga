71402eb4ea9ee2560fa644a32191a18c58eaf240
yelouafi <yelouafi@gmail.com>
2016-03-29 20:38:12 +0100

    removed logs

--- src/internal/asap.js
+++ src/internal/asap.js
@@ -1,24 +1,21 @@
 const queue = []
-let isExecuting = false
+let isSuspended = false
 
 export default function asap(task) {
-  //console.log('asap', isExecuting, queue)
-  if(!isExecuting) {
-    isExecuting = true
+  if(!isSuspended) {
+    isSuspended = true
     queue.push(task)
     asap.flush()
-    isExecuting = false
   } else {
     queue.push(task)
   }
 }
 
-asap.suspend = () => isExecuting = true
+asap.suspend = () => isSuspended = true
 asap.flush = () => {
-  //console.log('flush', isExecuting, queue)
   let nextTask
   while((nextTask = queue.shift())) {
     nextTask()
   }
-  isExecuting = false
+  isSuspended = false
 }
