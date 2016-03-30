e2af1916b978468f236aa3cd00df5d023ec909fc
yelouafi <yelouafi@gmail.com>
2016-03-30 11:17:58 +0100

    breaking change in middleware API

--- src/internal/middleware.js
+++ src/internal/middleware.js
@@ -1,4 +1,4 @@
-import { is, isDev, check, warnDeprecated } from './utils'
+import { is, isDev, check } from './utils'
 import asap from './asap'
 import proc from './proc'
 import emitter from './emitter'
@@ -10,31 +10,21 @@ export const sagaArgError = (fn, pos, saga) => (`
   Argument ${saga} at position ${pos} is not function!
 `)
 
-export const RUN_SAGA_DYNAMIC_ERROR = 'Before running a Saga dynamically using middleware.run, you must mount the Saga middleware on the Store using applyMiddleware'
+export const MIDDLEWARE_NOT_CONNECTED_ERROR = 'Before running a Saga, you must mount the Saga middleware on the Store using applyMiddleware'
 
-export const GET_STATE_DEPRECATED_WARNING = `
-  Using the 'getState' param of Sagas to access the state is deprecated since 0.9.1
-  To access the Store's state use 'yield select()' instead
-  For more infos see http://yelouafi.github.io/redux-saga/docs/api/index.html#selectselector-args
-`
-
-export default function sagaMiddlewareFactory(...sagas) {
+export default function sagaMiddlewareFactory() {
   let runSagaDynamically
 
-  sagas.forEach((saga, idx) => check(saga, is.func, sagaArgError('createSagaMiddleware', idx, saga)))
 
   function sagaMiddleware({getState, dispatch}) {
+    runSagaDynamically = runSaga
     const sagaEmitter = emitter()
     const monitor = isDev ? action => asap(() => dispatch(action)) : undefined
 
-    const getStateDeprecated = () => {
-      warnDeprecated(GET_STATE_DEPRECATED_WARNING)
-      return getState()
-    }
 
     function runSaga(saga, ...args) {
       return proc(
-        saga(getStateDeprecated, ...args),
+        saga(...args),
         sagaEmitter.subscribe,
         dispatch,
         getState,
@@ -44,25 +34,19 @@ export default function sagaMiddlewareFactory(...sagas) {
       )
     }
 
-    runSagaDynamically = runSaga
-
-    asap.suspend()
-    Promise.resolve().then(asap.flush)
-    sagas.forEach(runSaga)
-
     return next => action => {
       const result = next(action) // hit reducers
       // filter out monitor actions to avoid endless loops
       // see https://github.com/yelouafi/redux-saga/issues/61
       if(!action[MONITOR_ACTION])
-        asap(() => sagaEmitter.emit(action))
+        sagaEmitter.emit(action)
       return result;
     }
   }
 
   sagaMiddleware.run = (saga, ...args) => {
     if(!runSagaDynamically) {
-      throw new Error(RUN_SAGA_DYNAMIC_ERROR)
+      throw new Error(MIDDLEWARE_NOT_CONNECTED_ERROR)
     }
     check(saga, is.func, sagaArgError('sagaMiddleware.run', 0, saga))
 
