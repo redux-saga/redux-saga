c9c63d95a95846162a3f51739d2b9dee41bd695a
yelouafi <yelouafi@gmail.com>
2016-04-06 20:23:03 +0100

    fix issue takeEvery doesnt work when action type is a Symbol

--- src/internal/sagaHelpers.js
+++ src/internal/sagaHelpers.js
@@ -36,17 +36,17 @@ function fsmIterator(fsm, nextState, name = 'iterator') {
   if(typeof Symbol !== 'undefined') {
     iterator[Symbol.iterator] = () => iterator
   }
+
   return iterator
 }
 
 export function takeEvery(pattern, worker, ...args) {
   const yieldTake = { done: false, value: take(pattern)}
   const yieldFork = action => ({ done: false, value: fork(worker, ...args, action)})
-
   return fsmIterator({
     'take' : [yieldTake, 'fork'],
     'fork' : [yieldFork, 'take']
-  }, 'take', `takeEvery(${pattern}, ${worker.name})`)
+  }, 'take', `takeEvery(${String(pattern)}, ${worker.name})`)
 }
 
 export function takeLatest(pattern, worker, ...args) {
@@ -60,5 +60,5 @@ export function takeLatest(pattern, worker, ...args) {
     'take'   : [ yieldTake, forkOrCancel, action => currentAction = action ],
     'cancel' : [yieldCancel, 'fork'],
     'fork'   : [yieldFork, 'take', task => currentTask = task ]
-  }, 'take', `takeLatest(${pattern}, ${worker.name})`)
+  }, 'take', `takeLatest(${String(pattern)}, ${worker.name})`)
 }
