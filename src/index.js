46a7436e413751d30b11c9cea461b485adebdcb3
yelouafi <yelouafi@gmail.com>
2016-04-11 11:40:14 +0100

    export eventChannel and END

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import _SagaCancellationException from './internal/SagaCancellationException'
 export const SagaCancellationException = _SagaCancellationException
 export const isCancelError = error => error instanceof SagaCancellationException
 
-export { runSaga, storeIO } from './internal/runSaga'
-export { END, channel } from './internal/channel'
+export { runSaga } from './internal/runSaga'
+export { CANCEL } from './internal/proc'
+export { END, channel, eventChannel } from './internal/channel'
 export { takeEvery, takeLatest } from './internal/sagaHelpers'
 
 import * as effects from './effects'
