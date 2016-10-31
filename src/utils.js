d9313437824152411699e024d6f5aed757149b3e
yelouafi <yelouafi@gmail.com>
2016-10-31 12:28:20 +0000

    add exports

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,2 @@
-export { TASK, noop, is, deferred, arrayOfDeffered, createMockTask } from './internal/utils'
+export { TASK, SAGA_ACTION, noop, is, deferred, arrayOfDeffered, createMockTask } from './internal/utils'
 export { CHANNEL_END, asEffect } from './internal/io'
