2e8358a91aee622d5416147ff5156b10917f2991
nihau <nicolas.hauseux@gmail.com>
2017-02-17 20:36:03 +0100

    add utilitary function cloneableGenerator

--- src/internal/utils.js
+++ src/internal/utils.js
@@ -148,3 +148,21 @@ export function wrapSagaDispatch(dispatch) {
     return dispatch(wrappedAction)
   }
 }
+
+export const cloneableGenerator = (generatorFunc) => (...args) => {
+  const history = [];
+  const gen = generatorFunc(...args);
+  return {
+    next: (arg) => {
+      history.push(arg);
+      return gen.next(arg);
+    },
+    clone: () => {
+      const clonedGen = cloneableGenerator(generatorFunc)(...args);
+      history.forEach((arg) => clonedGen.next(arg));
+      return clonedGen;
+    },
+    return: (value) => gen.return(value),
+    throw: (exception) => gen.throw(exception)
+  };
+};
