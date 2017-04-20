f345547ea061a25e54f89463d9c02ee1018fc63e
Mateusz Burzyński <mateuszburzynski@gmail.com>
2017-04-20 10:15:15 +0200

    Updated dist files

--- dist/redux-saga.js
+++ dist/redux-saga.js
@@ -1961,7 +1961,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    suspend();
 	    task();
 	  } finally {
-	    flush();
+	    release();
 	  }
 	}
 
@@ -1969,10 +1969,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	  Executes or queues a task depending on the state of the scheduler (`suspended` or `released`)
 	**/
 	function asap(task) {
+	  queue.push(task);
+
 	  if (!semaphore) {
-	    exec(task);
-	  } else {
-	    queue.push(task);
+	    suspend();
+	    flush();
 	  }
 	}
 
@@ -1985,12 +1986,21 @@ return /******/ (function(modules) { // webpackBootstrap
 	}
 
 	/**
+	  Puts the scheduler in a `released` state.
+	**/
+	function release() {
+	  semaphore--;
+	}
+
+	/**
 	  Releases the current lock. Executes all queued tasks if the scheduler is in the released state.
 	**/
 	function flush() {
-	  semaphore--;
-	  if (!semaphore && queue.length) {
-	    exec(queue.shift());
+	  release();
+
+	  var task = void 0;
+	  while (!semaphore && (task = queue.shift()) !== undefined) {
+	    exec(task);
 	  }
 	}
 
