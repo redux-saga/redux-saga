154f37636eb9027c731256ca266935d008eb2230
Sergey <sse1595@gmail.com>
2018-07-10 15:57:23 +0300

    takeLatest parameter error should be more specific: Issue #1241 (#1284)
    * Issue #1241
    
    * Check worker at takeLates, takeEvery and throttle
    
    * move validation to io.js
    
    * Merge branch 'master' of https://github.com/redux-saga/redux-saga
    
    # Conflicts:
    #	packages/core/src/internal/io.js
    
    * Merge branch 'master' of https://github.com/redux-saga/redux-saga
    
    # Conflicts:
    #	packages/core/src/internal/io.js
    
    * revert package-lock

--- packages/core/src/internal/io-helpers.js
+++ packages/core/src/internal/io-helpers.js
@@ -1,19 +1,49 @@
 import { call, fork } from './io'
-import { takeEveryHelper, takeLatestHelper, takeLeadingHelper, throttleHelper, retryHelper, debounceHelper } from './sagaHelpers'
+import { check, is } from './utils'
+import {
+  takeEveryHelper,
+  takeLatestHelper,
+  takeLeadingHelper,
+  throttleHelper,
+  retryHelper,
+  debounceHelper,
+} from './sagaHelpers'
+
+const validateTakeEffect = (fn, patternOrChannel, worker) => {
+  check(patternOrChannel, is.notUndef, `${fn.name} requires a pattern or channel`)
+  check(worker, is.notUndef, `${fn.name} requires a saga parameter`)
+}
 
 export function takeEvery(patternOrChannel, worker, ...args) {
+  if (process.env.NODE_ENV === 'development') {
+    validateTakeEffect(takeEvery, patternOrChannel, worker)
+  }
+
   return fork(takeEveryHelper, patternOrChannel, worker, ...args)
 }
 
 export function takeLatest(patternOrChannel, worker, ...args) {
+  if (process.env.NODE_ENV === 'development') {
+    validateTakeEffect(takeLatest, patternOrChannel, worker)
+  }
+
   return fork(takeLatestHelper, patternOrChannel, worker, ...args)
 }
 
 export function takeLeading(patternOrChannel, worker, ...args) {
+  if (process.env.NODE_ENV === 'development') {
+    validateTakeEffect(takeLeading, patternOrChannel, worker)
+  }
+
   return fork(takeLeadingHelper, patternOrChannel, worker, ...args)
 }
 
 export function throttle(ms, pattern, worker, ...args) {
+  if (process.env.NODE_ENV === 'development') {
+    check(pattern, is.notUndef, 'throttle requires a pattern')
+    check(worker, is.notUndef, 'throttle requires a saga parameter')
+  }
+
   return fork(throttleHelper, ms, pattern, worker, ...args)
 }
 
