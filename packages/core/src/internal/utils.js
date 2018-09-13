1f08b293e72cb269aabd1e3be3bbee4a4fdb4778
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-09-14 00:08:06 +0200

    Add dev-only warning when saga gets used with async generator

--- packages/core/src/internal/utils.js
+++ packages/core/src/internal/utils.js
@@ -8,6 +8,9 @@ export const kFalse = konst(false)
 export const noop = () => {}
 export const identity = v => v
 
+const hasSymbol = typeof Symbol === 'function'
+export const asyncIteratorSymbol = hasSymbol && Symbol.asyncIterator ? Symbol.asyncIterator : '@@asyncIterator'
+
 export function check(value, predicate, error) {
   if (!predicate(value)) {
     throw new Error(error)
