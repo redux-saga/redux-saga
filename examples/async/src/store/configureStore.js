00742351e5e4d4f86007961c11765617bde49015
restrry <restrry@gmail.com>
2018-10-24 09:50:13 +0200

    examples: support codesandbox integration

--- examples/async/src/store/configureStore.js
+++ examples/async/src/store/configureStore.js
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers'
 import createSagaMiddleware from 'redux-saga'
-import sagaMonitor from '../../../sagaMonitor'
 
 export default function configureStore() {
-  const sagaMiddleware = createSagaMiddleware({ sagaMonitor })
+  const sagaMiddleware = createSagaMiddleware()
   return {
     ...createStore(rootReducer, applyMiddleware(sagaMiddleware)),
     runSaga: sagaMiddleware.run,
---
+++ examples/shopping-cart/README.md
@@ -0,0 +1,5 @@
+# Run locally
+`npm start`
+
+# Open in browser
+[Click here](https://codesandbox.io/s/github/redux-saga/redux-saga/tree/master/examples/shopping-cart)
