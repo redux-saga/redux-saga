dff79a5f9b737f821b71f7f97292c24e702b28cc
肥超 <842351815@qq.com>
2018-11-26 19:36:21 +0800

    Use jest.useFakeTimer instead of lolex. (#1687)
    * Use jest.useFakeTimer instead of lolex.
    
    * tests: Call jest.useRealTimers before exiting throttle test.

--- packages/core/__tests__/sagaHelpers/throttle.js
+++ packages/core/__tests__/sagaHelpers/throttle.js
@@ -1,10 +1,11 @@
-import lolex from 'lolex'
 import sagaMiddleware, { END } from '../../src'
 import { createStore, applyMiddleware } from 'redux'
 import delayP from '@redux-saga/delay-p'
 import { take, cancel, throttle } from '../../src/effects'
+
 test('throttle', () => {
-  const clock = lolex.install()
+  jest.useFakeTimers()
+
   const actual = []
   const expected = [['a1', 'a2', 0], ['a1', 'a2', 10], ['a1', 'a2', 20], ['a1', 'a2', 30], ['a1', 'a2', 34]]
   const middleware = sagaMiddleware()
@@ -26,48 +27,33 @@ test('throttle', () => {
   for (let i = 0; i < 35; i++) {
     dispatchedActions.push(
       delayP(i * 10)
-        .then(() => i)
-        .then(val =>
-          store.dispatch({
-            type: 'ACTION',
-            payload: val,
-          }),
-        )
-        .then(() => clock.tick(10)), // next tick
+        .then(() => store.dispatch({ type: 'ACTION', payload: i }))
+        .then(() => jest.advanceTimersByTime(10)), // next tick
     )
   }
 
   Promise.resolve()
-    .then(() => clock.tick(1)) // just start for the smallest tick
-    .then(() => clock.tick(10)) // tick past first delay
+    .then(() => jest.advanceTimersByTime(1)) // just start for the smallest tick
+    .then(() => jest.advanceTimersByTime(10)) // tick past first delay
 
-  return dispatchedActions[34] // wait so trailing dispatch gets processed
-    .then(() => clock.tick(100))
-    .then(() =>
-      store.dispatch({
-        type: 'CANCEL_WATCHER',
-      }),
-    ) // shouldn't be processed cause of getting canceled
-    .then(() =>
-      store.dispatch({
-        type: 'ACTION',
-        payload: 40,
-      }),
-    )
-    .then(() => {
-      // throttle must ignore incoming actions during throttling interval
-      expect(actual).toEqual(expected)
-    })
-    .then(
-      () => {
-        clock.uninstall()
-      },
-      err => {
-        clock.uninstall()
+  return (
+    dispatchedActions[34] // wait so trailing dispatch gets processed
+      .then(() => jest.advanceTimersByTime(100))
+      .then(() => store.dispatch({ type: 'CANCEL_WATCHER' }))
+      // shouldn't be processed cause of getting canceled
+      .then(() => store.dispatch({ type: 'ACTION', payload: 40 }))
+      .then(() => {
+        // throttle must ignore incoming actions during throttling interval
+        expect(actual).toEqual(expected)
+        jest.useRealTimers()
+      })
+      .catch(err => {
+        jest.useRealTimers()
         throw err
-      },
-    )
+      })
+  )
 })
+
 test('throttle: pattern END', () => {
   const delayMs = 20
   const middleware = sagaMiddleware()
