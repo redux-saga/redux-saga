01955d0e5f99f71043ceb01a43a4888ff53565fe
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-02-02 09:43:10 +0100

    Fix lerna alias in testing-utils

--- packages/testing-utils/jest.config.js
+++ packages/testing-utils/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   testEnvironment: 'node',
   moduleNameMapper: Object.assign(lernaAliases, {
     '^redux-saga/effects$': lernaAliases['^redux-saga$'].replace(/index\.js$/, 'effects.js'),
+    '^@redux-saga/core/effects$': lernaAliases['^@redux-saga/core$'].replace(/index\.js$/, 'effects.js'),
   }),
   transform: {
     '.js$': __dirname + '/babel-transformer.jest.js',
