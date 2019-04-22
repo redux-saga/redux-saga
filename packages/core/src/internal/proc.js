a265e07b1c3d9be694be91bdbaab81afe943a37a
szb512 <47613831+szb512@users.noreply.github.com>
2019-04-22 05:37:00 -0400

    Flatten stack experiment (#148)
    * WIP next from recursion to iteration
    
    * dont use array as queue

--- packages/core/src/internal/proc.js
+++ packages/core/src/internal/proc.js
@@ -143,6 +143,26 @@ export default function proc(env, iterator, parentContext, parentEffectId, meta,
   return task
 
   /**
+    This is the generator driver
+    It's a recursive async/continuation function which calls itself
+    until the generator terminates or throws
+  **/
+
+  function next(_arg, _isErr) {
+    let resultPointer = {
+      arg: _arg,
+      isErr: _isErr,
+    }
+
+    // Preventive measure. If we end up here, then there is really something wrong
+    if (!mainTask._isRunning) {
+      throw new Error('Trying to resume an already finished generator')
+    }
+
+    let isSync = false
+    function nextWrapper(arg, isErr) {
+      if (isSync) {
+        resultPointer = { arg, isErr }
    * This is the generator driver
    * It's a recursive async/continuation function which calls itself
    * until the generator terminates or throws
@@ -180,9 +200,63 @@ export default function proc(env, iterator, parentContext, parentEffectId, meta,
         // We get TERMINATE flag, i.e. by taking from a channel that ended using `take` (and not `takem` used to trap End of channels)
         result = is.func(iterator.return) ? iterator.return() : { done: true }
       } else {
-        result = iterator.next(arg)
+        next(arg, isErr)
       }
+    }
 
+    // temporary workaround
+    Object.defineProperty(nextWrapper, 'cancel', {
+      get: function() {
+        return next.cancel
+      },
+      set: function(fn) {
+        next.cancel = fn
+      },
+    })
+
+    try {
+      while (resultPointer) {
+        const { arg, isErr } = resultPointer
+        resultPointer = null
+        let result
+        if (isErr) {
+          result = iterator.throw(arg)
+        } else if (shouldCancel(arg)) {
+          /**
+            getting TASK_CANCEL automatically cancels the main task
+            We can get this value here
+
+            - By cancelling the parent task manually
+            - By joining a Cancelled task
+          **/
+          mainTask._isCancelled = true
+          /**
+            Cancels the current effect; this will propagate the cancellation down to any called tasks
+          **/
+          nextWrapper.cancel()
+          /**
+            If this Generator has a `return` method then invokes it
+            This will jump to the finally block
+          **/
+          result = is.func(iterator.return) ? iterator.return(TASK_CANCEL) : { done: true, value: TASK_CANCEL }
+        } else if (shouldTerminate(arg)) {
+          // We get TERMINATE flag, i.e. by taking from a channel that ended using `take` (and not `takem` used to trap End of channels)
+          result = is.func(iterator.return) ? iterator.return() : { done: true }
+        } else {
+          result = iterator.next(arg)
+        }
+
+        if (!result.done) {
+          isSync = true // true
+          digestEffect(result.value, parentEffectId, '', nextWrapper)
+          isSync = false
+        } else {
+          /**
+            This Generator has ended, terminate the main task and notify the fork queue
+          **/
+          mainTask._isRunning = false
+          mainTask.cont(result.value)
+        }
       if (!result.done) {
         digestEffect(result.value, parentEffectId, next)
       } else {
