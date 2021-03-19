24e9a68d621fd2a57a29b3b80e4b54ecb22fa593
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-03-19 07:55:04 +0100

    Fix rendering backers & sponsors on the site

--- website/src/components/Supporters.jsx
+++ website/src/components/Supporters.jsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-const BACKER_COUNT = 30;
-const SPONSOR_COUNT = 30;
+const BACKER_COUNT = 30
+const SPONSOR_COUNT = 30
 
 function Supporters({ type }) {
-  const COUNT = type === 'backer' ? BACKER_COUNT : SPONSOR_COUNT;
+  const COUNT = type === 'backer' ? BACKER_COUNT : SPONSOR_COUNT
 
   if (type !== 'backer' && type !== 'sponsor') {
-    throw new Error('Invalid supporter type');
+    throw new Error('Invalid supporter type')
   }
 
-  const supporters = [...new Array(COUNT)].map((v, i) => (
+  const supporters = Array.from({ length: COUNT }).map((v, i) => (
     // Ok to use index as key here, elements will not change
-    <a key={i} href={`https://opencollective.com/redux-saga/${type}/${i}/website`} className={type + ' supporter col'} target="_blank" rel="noreferrer">
+    <a
+      key={i}
+      href={`https://opencollective.com/redux-saga/${type}/${i}/website`}
+      className={type + ' supporter col'}
+      target="_blank"
+      rel="noreferrer noopener"
+    >
       <img src={`https://opencollective.com/redux-saga/${type}/${i}/avatar.svg`} />
     </a>
-  ));
+  ))
 
-  return (
-    <>
-      {supporters}
-    </>
-  );
+  return <>{supporters}</>
 }
 
 Supporters.propTypes = {
   type: PropTypes.string,
-};
+}
 
-export default Supporters;
+export default Supporters
