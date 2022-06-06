2d2214e9ca8949892c0a7a23ceef39fa32d13939
Eric Bower <me@erock.io>
2022-06-06 14:25:34 -0400

    Fixed an issue with arguments that exceed the maximum value for the internally-used `setTimeout` (#2293)

--- packages/core/__tests__/sagaHelpers/delay.js
+++ packages/core/__tests__/sagaHelpers/delay.js
@@ -1,6 +1,6 @@
 import sagaMiddleware from '../../src'
 import { createStore, applyMiddleware } from 'redux'
-import { delay } from '../../src/effects'
+import { delay, call } from '../../src/effects'
 import delayP from '@redux-saga/delay-p'
 
 test('delay', async () => {
@@ -19,4 +19,27 @@ test('delay', async () => {
   await delayP(100)
 
   expect(actual).toEqual(expected)
+})
+
+test('delay when the timeout value exceeds the maximum allowed value', () => {
+  let actual
+  const middleware = sagaMiddleware({
+    onError: err => {
+      actual = err
+    },
+  })
+  createStore(() => ({}), {}, applyMiddleware(middleware))
+
+  function* child() {
+    yield delay(2147483648)
+  }
+
+  function* main() {
+    yield call(child)
+  }
+
+  const task = middleware.run(main)
+  return task.toPromise().catch(() => {
+    expect(actual).toEqual(new Error('delay only supports a maximum value of 2147483647ms'))
+  })
 })
--- packages/delay-p/src/index.js
+++ packages/delay-p/src/index.js
@@ -1,9 +1,15 @@
 import { CANCEL } from '@redux-saga/symbols'
 
+const MAX_SIGNED_INT = 2147483647
+
 export default function delayP(ms, val = true) {
+  // https://developer.mozilla.org/en-US/docs/Web/API/setTimeout#maximum_delay_value
+  if (process.env.NODE_ENV !== 'production' && ms > MAX_SIGNED_INT) {
+    throw new Error('delay only supports a maximum value of ' + MAX_SIGNED_INT + 'ms')
+  }
   let timeoutId
   const promise = new Promise(resolve => {
-    timeoutId = setTimeout(resolve, ms, val)
+    timeoutId = setTimeout(resolve, Math.min(MAX_SIGNED_INT, ms), val)
   })
 
   promise[CANCEL] = () => {
