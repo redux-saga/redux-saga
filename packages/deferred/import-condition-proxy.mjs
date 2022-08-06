4c848cf4e6913605255e286d2e66612489590363
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-08-06 09:56:06 +0200

    Add `package.json#exports` to more packages and introduce `import-condition-proxy.mjs` where needed

---
+++ packages/deferred/import-condition-proxy.mjs
@@ -0,0 +1,4 @@
+export * from './dist/redux-saga-deferred.cjs.js'
+import _default from './dist/redux-saga-deferred.cjs.js'
+
+export default _default.default
---
+++ packages/redux-saga/import-condition-proxy.mjs
@@ -0,0 +1,4 @@
+export * from './dist/redux-saga-core-npm-proxy.cjs.js'
+import _default from './dist/redux-saga-core-npm-proxy.cjs.js'
+
+export default _default.default
