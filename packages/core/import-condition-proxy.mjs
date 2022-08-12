8207e33be4379d8cf5d331d4578341e87dfeb86f
Eric Bower <me@erock.io>
2022-08-12 06:49:15 -0400

    `exports` field has been added to the `package.json` manifests (#2308)
    * add package entry points
    
    * Add `package.json#exports` to more packages and introduce `import-condition-proxy.mjs` where needed
    
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

---
+++ packages/core/import-condition-proxy.mjs
@@ -0,0 +1,4 @@
+export * from './dist/redux-saga-core.cjs.js'
+import _default from './dist/redux-saga-core.cjs.js'
+
+export default _default.default
---
+++ packages/delay-p/import-condition-proxy.mjs
@@ -0,0 +1,4 @@
+export * from './dist/redux-saga-delay-p.cjs.js'
+import _default from './dist/redux-saga-delay-p.cjs.js'
+
+export default _default.default
