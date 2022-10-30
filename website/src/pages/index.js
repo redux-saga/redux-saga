7536df67005bcbc1e5dd1e7e23094af464aa0952
David Lee <lihongyi0512@gmail.com>
2022-10-30 23:23:38 +0800

    add a new snippet in src/pages/index.js

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from 'react'
 
-import Layout from '@theme/Layout';
-import Tabs from '@theme/Tabs';
-import TabItem from '@theme/TabItem';
-import Header from '../components/Header';
-import Feature from '../components/Feature';
-import CodeSnippet from '../components/CodeSnippet';
-import Supporters from '../components/Supporters';
+import Layout from '@theme/Layout'
+import Tabs from '@theme/Tabs'
+import TabItem from '@theme/TabItem'
+import Header from '../components/Header'
+import Feature from '../components/Feature'
+import CodeSnippet from '../components/CodeSnippet'
+import Supporters from '../components/Supporters'
 
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 
-import styles from './styles.module.css';
+import styles from './styles.module.css'
 
 const features = [
   {
@@ -34,18 +34,23 @@ const features = [
       Either way, side effect testing is quick, concise, and painless, as testing should be.
     `,
   },
-];
+]
 
 const snippets = [
   {
     label: '1. Dispatch an action',
-    details: <>Suppose we have a UI to fetch some user data from a remote server when a button is clicked. (For brevity, we&apos;ll just show the action triggering code.)</>,
+    details: (
+      <>
+        Suppose we have a UI to fetch some user data from a remote server when a button is clicked. (For brevity,
+        we&apos;ll just show the action triggering code.)
+      </>
+    ),
     code: [
       'class UserComponent extends React.Component {',
       '  ...',
       '  onSomeButtonClicked() {',
       '    const { userId, dispatch } = this.props',
-      '    dispatch({type: \'USER_FETCH_REQUESTED\', payload: {userId}})',
+      "    dispatch({type: 'USER_FETCH_REQUESTED', payload: {userId}})",
       '  }',
       '  ...',
       '}',
@@ -53,10 +58,15 @@ const snippets = [
   },
   {
     label: '2. Initiate a side effect',
-    details: <>The component dispatches a plain object action to the store. We&apos;ll create a Saga that watches for all <code>USER_FETCH_REQUESTED</code> actions and triggers an API call to fetch the user data.</>,
+    details: (
+      <>
+        The component dispatches a plain object action to the store. We&apos;ll create a Saga that watches for all{' '}
+        <code>USER_FETCH_REQUESTED</code> actions and triggers an API call to fetch the user data.
+      </>
+    ),
     code: [
-      'import { call, put, takeEvery, takeLatest } from \'redux-saga/effects\'',
-      'import Api from \'...\'\n',
+      "import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'",
+      "import Api from '...'\n",
       '// Worker saga will be fired on USER_FETCH_REQUESTED actions',
       'function* fetchUser(action) {',
       '   try {',
@@ -75,12 +85,16 @@ const snippets = [
   },
   {
     label: '3. Connect to the store',
-    details: <>To run our Saga, we have to connect it to the Redux store using the <code>redux-saga</code> middleware.</>,
+    details: (
+      <>
+        To run our Saga, we have to connect it to the Redux store using the <code>redux-saga</code> middleware.
+      </>
+    ),
     code: [
-      'import { createStore, applyMiddleware } from \'redux\'',
-      'import createSagaMiddleware from \'redux-saga\'\n',
-      'import reducer from \'./reducers\'',
-      'import mySaga from \'./sagas\'\n',
+      "import { createStore, applyMiddleware } from 'redux'",
+      "import createSagaMiddleware from 'redux-saga'\n",
+      "import reducer from './reducers'",
+      "import mySaga from './sagas'\n",
       '// Create the saga middleware',
       'const sagaMiddleware = createSagaMiddleware()',
       '// Mount it on the Store',
@@ -93,16 +107,44 @@ const snippets = [
       '// Render the application',
     ].join('\n'),
   },
-];
+  {
+    label: '4. Connect to the store (new version)',
+    details: (
+      <>
+        This is the new version of running saga by using configureStore from <code>reduxjs/toolkit</code> instead of
+        createStore.
+      </>
+    ),
+    code: [
+      "import { configureStore } from '@reduxjs/toolkit'",
+      "import createSagaMiddleware from 'redux-saga'\n",
+      "import reducer from './reducers'",
+      "import mySaga from './sagas'\n",
+      '// Create the saga middleware',
+      'const sagaMiddleware = createSagaMiddleware()',
+      'const middleware = [sagaMiddleware]',
+      '// Mount it on the Store',
+      'const store = configureStore({',
+      '  reducer,',
+      '  middleware: (getDefaultMiddleware) =>',
+      '      getDefaultMiddleware().concat(middleware),',
+      '})\n',
+      '// Then run the saga',
+      'sagaMiddleware.run(mySaga)\n',
+      '// Render the application',
+    ].join('\n'),
+  },
+]
 
 function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const context = useDocusaurusContext()
+  const { siteConfig = {} } = context
 
   return (
     <Layout
       title={`${siteConfig.title} - ${siteConfig.tagline}`}
-      description="An open source Redux middleware library for efficiently handling asynchronous side effects">
+      description="An open source Redux middleware library for efficiently handling asynchronous side effects"
+    >
       <Header />
       <main>
         <section className={'feature ' + styles.features}>
@@ -138,12 +180,18 @@ function Home() {
             <div className="row">
               <div className="col col--6">
                 <h1>Backers</h1>
-                <p>Support us with a monthly donation and help us continue our activities. <a href="https://opencollective.com/redux-saga#backer">Become a backer</a></p>
+                <p>
+                  Support us with a monthly donation and help us continue our activities.{' '}
+                  <a href="https://opencollective.com/redux-saga#backer">Become a backer</a>
+                </p>
                 <Supporters type="backer" />
               </div>
               <div className="col col--6">
                 <h1>Sponsors</h1>
-                <p>Become a sponsor and have your logo shown below and on Github with a link to your site. <a href="https://opencollective.com/redux-saga#sponsor">Become a sponsor</a></p>
+                <p>
+                  Become a sponsor and have your logo shown below and on Github with a link to your site.{' '}
+                  <a href="https://opencollective.com/redux-saga#sponsor">Become a sponsor</a>
+                </p>
                 <Supporters type="sponsor" />
               </div>
             </div>
@@ -151,7 +199,7 @@ function Home() {
         </section>
       </main>
     </Layout>
-  );
+  )
 }
 
-export default Home;
+export default Home
