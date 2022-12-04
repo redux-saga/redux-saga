ddde330d3a278a411966799d1e6647b33f194a13
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-12-04 14:27:25 +0100

    Merge two code branches in `throttle`

--- packages/core/src/internal/sagaHelpers/throttle.js
+++ packages/core/src/internal/sagaHelpers/throttle.js
@@ -7,49 +7,35 @@ export default function throttle(delayLength, patternOrChannel, worker, ...args)
   let action, channel
 
   const yTake = () => ({ done: false, value: take(channel) })
-  const yFork = (ac) => ({ done: false, value: fork(worker, ...args, ac) })
+  const yFork = ac => ({ done: false, value: fork(worker, ...args, ac) })
   const yDelay = { done: false, value: delay(delayLength) }
 
-  const setAction = (ac) => (action = ac)
-  const setChannel = (ch) => (channel = ch)
+  const setAction = ac => (action = ac)
+  const setChannel = ch => (channel = ch)
 
-  if (is.channel(patternOrChannel)) {
-    channel = patternOrChannel
-    return fsmIterator(
-      {
-        q1() {
-          return { nextState: 'q2', effect: yTake(), stateUpdater: setAction }
-        },
-        q2() {
-          return { nextState: 'q3', effect: yFork(action) }
-        },
-        q3() {
-          return { nextState: 'q1', effect: yDelay }
-        },
-      },
-      'q1',
-      `throttle(${safeName(patternOrChannel)}, ${worker.name})`,
-    )
-  } else {
-    const yActionChannel = { done: false, value: actionChannel(patternOrChannel, buffers.sliding(1)) }
+  const needsChannel = !is.channel(patternOrChannel)
 
-    return fsmIterator(
-      {
-        q1() {
-          return { nextState: 'q2', effect: yActionChannel, stateUpdater: setChannel }
-        },
-        q2() {
-          return { nextState: 'q3', effect: yTake(), stateUpdater: setAction }
-        },
-        q3() {
-          return { nextState: 'q4', effect: yFork(action) }
-        },
-        q4() {
-          return { nextState: 'q2', effect: yDelay }
-        },
-      },
-      'q1',
-      `throttle(${safeName(patternOrChannel)}, ${worker.name})`,
-    )
+  if (!needsChannel) {
+    setChannel(patternOrChannel)
   }
+
+  return fsmIterator(
+    {
+      q1() {
+        const yActionChannel = { done: false, value: actionChannel(patternOrChannel, buffers.sliding(1)) }
+        return { nextState: 'q2', effect: yActionChannel, stateUpdater: setChannel }
+      },
+      q2() {
+        return { nextState: 'q3', effect: yTake(), stateUpdater: setAction }
+      },
+      q3() {
+        return { nextState: 'q4', effect: yFork(action) }
+      },
+      q4() {
+        return { nextState: 'q2', effect: yDelay }
+      },
+    },
+    needsChannel ? 'q1' : 'q2',
+    `throttle(${safeName(patternOrChannel)}, ${worker.name})`,
+  )
 }
