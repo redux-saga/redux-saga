523362dbdaf4334c5aec96acccc15d850cfa7a6f
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-05-24 16:20:24 +0200

    Cleanup files, scripts and more before using Preconstruct

--- packages/core/import-condition-proxy.mjs
+++
@@ -1,4 +0,0 @@
-export * from './dist/redux-saga-core.cjs.js'
-import _default from './dist/redux-saga-core.cjs.js'
-
-export default _default.default
--- packages/delay-p/import-condition-proxy.mjs
+++
@@ -1,4 +0,0 @@
-export * from './dist/redux-saga-delay-p.cjs.js'
-import _default from './dist/redux-saga-delay-p.cjs.js'
-
-export default _default.default
