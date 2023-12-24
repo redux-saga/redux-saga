c7f387d7a8e1d6f48768877e0de9a984f6cf2681
chensw <swchenforgetful@hotmail.com>
2023-12-25 04:06:54 +0800

    refa: make scheduler easier to understand

--- packages/core/__tests__/scheduler.js
+++ packages/core/__tests__/scheduler.js
@@ -29,3 +29,55 @@ test('scheduler when suspended queues up and executes all tasks on flush', () =>
   })
   expect(actual).toEqual(['1', '2', '3'])
 })
+
+test('scheduler queues a task and flush1', () => {
+  const actual = []
+  asap(() => {
+    actual.push('1')
+    asap(() => {
+      actual.push('3')
+    })
+    actual.push('2')
+  })
+  expect(actual).toEqual(['1', '2', '3'])
+})
+
+test('scheduler queues a task and flush2', () => {
+  const actual = []
+  asap(() => {
+    asap(() => {
+      asap(() => {
+        actual.push('3')
+      })
+      actual.push('2')
+    })
+    actual.push('1')
+  })
+  expect(actual).toEqual(['1', '2', '3'])
+})
+
+test('scheduler queues a task and flush3', () => {
+  const actual = []
+  immediately(() => {
+    actual.push('1')
+    asap(() => {
+      actual.push('3')
+    })
+    actual.push('2')
+  })
+  expect(actual).toEqual(['1', '2', '3'])
+})
+
+test('scheduler queues a task and flush4', () => {
+  const actual = []
+  immediately(() => {
+    asap(() => {
+      asap(() => {
+        actual.push('3')
+      })
+      actual.push('2')
+    })
+    actual.push('1')
+  })
+  expect(actual).toEqual(['1', '2', '3'])
+})
--- packages/core/src/internal/scheduler.js
+++ packages/core/src/internal/scheduler.js
@@ -9,44 +9,6 @@ const queue = []
 let semaphore = 0
 
 /**
-  Executes a task 'atomically'. Tasks scheduled during this execution will be queued
-  and flushed after this task has finished (assuming the scheduler endup in a released
-  state).
-**/
-function exec(task) {
-  try {
-    suspend()
-    task()
-  } finally {
-    release()
-  }
-}
-
-/**
-  Executes or queues a task depending on the state of the scheduler (`suspended` or `released`)
-**/
-export function asap(task) {
-  queue.push(task)
-
-  if (!semaphore) {
-    suspend()
-    flush()
-  }
-}
-
-/**
- * Puts the scheduler in a `suspended` state and executes a task immediately.
- */
-export function immediately(task) {
-  try {
-    suspend()
-    return task()
-  } finally {
-    flush()
-  }
-}
-
-/**
   Puts the scheduler in a `suspended` state. Scheduled tasks will be queued until the
   scheduler is released.
 **/
@@ -62,11 +24,42 @@ function release() {
 }
 
 /**
-  Releases the current lock. Executes all queued tasks if the scheduler is in the released state.
+  Executes a task 'atomically'. Tasks scheduled during this execution will be queued
+  and flushed after this task has finished (assuming the scheduler endup in a released
+  state).
+**/
+function exec(task) {
+  try {
+    suspend()
+    return task()
+  } finally {
+    release()
+  }
+}
+
+/**
+  Queues a task and flush.
+**/
+export function asap(task) {
+  queue.push(task)
+  flush()
+}
+
+/**
+  Executes a task immediately and flush.
+ */
+export function immediately(task) {
+  try {
+    return exec(task)
+  } finally {
+    flush()
+  }
+}
+
+/**
+  Executes all queued tasks if the scheduler is in the released state.
 **/
 function flush() {
-  release()
-
   let task
   while (!semaphore && (task = queue.shift()) !== undefined) {
     exec(task)
