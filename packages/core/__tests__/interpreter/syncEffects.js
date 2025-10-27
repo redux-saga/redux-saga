070539db97d24a3bb6019a99286eecf46c54ffed
Mateusz Burzyński <mateuszburzynski@gmail.com>
2025-10-27 22:16:15 +0100

    wip

--- packages/core/__tests__/interpreter/syncEffects.js
+++ packages/core/__tests__/interpreter/syncEffects.js
@@ -91,6 +91,37 @@ test('suceeds on a long synchronous loop through fork effects', () => {
   })
 })
 
+test('suceeds on a long synchronous loop through fork effects after async resolution', () => {
+  const callAttempts = 1500
+  let actualCallCount = 0
+
+  const rootReducer = () => ({})
+
+  const middleware = sagaMiddleware()
+  createStore(rootReducer, {}, applyMiddleware(middleware))
+
+  function* genFnChild() {
+    actualCallCount++
+  }
+
+  function* genFnParent() {
+    yield Promise.resolve()
+
+    for (let i = 0; i < callAttempts; i++) {
+      yield io.fork(genFnChild)
+    }
+  }
+
+  function* genWrapper() {
+    yield io.call(genFnParent)
+  }
+
+  const task = middleware.run(genWrapper)
+  return task.toPromise().then(() => {
+    expect(actualCallCount).toBe(callAttempts)
+  })
+})
+
 test('suceeds on a long synchronous loop through put effects', () => {
   const callAttempts = 1500
 
@@ -109,6 +140,41 @@ test('suceeds on a long synchronous loop through put effects', () => {
   const store = createStore(rootReducer, { callCount: 0 }, applyMiddleware(middleware))
 
   function* genFnParent() {
+    for (let i = 0; i < callAttempts; i++) {
+      yield io.put({ type: 'INC' })
+    }
+  }
+
+  function* genWrapper() {
+    yield io.call(genFnParent)
+  }
+
+  const task = middleware.run(genWrapper)
+  return task.toPromise().then(() => {
+    expect(store.getState().callCount).toBe(callAttempts)
+  })
+})
+
+test('suceeds on a long synchronous loop through put effects after async resolution', () => {
+  const callAttempts = 1500
+
+  const rootReducer = (state, action) => {
+    switch (action.type) {
+      case 'INC':
+        return {
+          ...state,
+          callCount: state.callCount + 1,
+        }
+    }
+    return state
+  }
+
+  const middleware = sagaMiddleware()
+  const store = createStore(rootReducer, { callCount: 0 }, applyMiddleware(middleware))
+
+  function* genFnParent() {
+    yield Promise.resolve()
+
     for (let i = 0; i < callAttempts; i++) {
       yield io.put({ type: 'INC' })
     }
