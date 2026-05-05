bdd5699710d0ac95b4640663d9513e8aad1c2a01
Yassine EL OUAFI <yelouafi@gmail.com>
2026-05-05 17:12:13 +0100

    Address UMD build review nit

--- scripts/build-redux-saga-umd.js
+++ scripts/build-redux-saga-umd.js
@@ -1,4 +1,4 @@
-const fs = require('fs').promises
+const fs = require('fs/promises')
 const path = require('path')
 const { rollup } = require('rollup')
 const alias = require('rollup-plugin-alias')
